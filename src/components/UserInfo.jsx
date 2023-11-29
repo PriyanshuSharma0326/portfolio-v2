2825d324b41633abbc689858edd4a7cb258e08b4
PriyanshuSharma0326 <ps940016@gmail.com>
2023-11-29 20:08:37 +0530

    Update cv button

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -157,7 +157,7 @@ function UserInfo() {
                     </p>
                 </div>
 
-                <div className="
+                <a className="
                     download-cv 
                     px-8 
                     mt-8
@@ -172,6 +172,9 @@ function UserInfo() {
                     
                     max-[426px]:px-4
                     max-[426px]:mt-[3.5rem]"
+                    href='https://s3.amazonaws.com/attachments.angel.co/9483843-af16b72a2ac5ae56a6a9be3b06dd5ab3.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIATAVHNKYQ6AA22R77%2F20231129%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231129T142800Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEH4aCXVzLXdlc3QtMiJGMEQCIB%2F68qpj%2FqiozRbjwIS%2Fuw5OVmAQl5IZOr3Lbs%2Bkq4kqAiB6r2upNA6x13rHbX9aBv0kXWibsegYRJTeaCLc5V0IjiqTBQjX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAAaDDIwNzU4MzI3MDQzMyIMiTDlW%2ByToXNei2FtKucEN6pKiiTsRdhERP5mHgDxof74iBp8ztFi0XLzZ8vw0ugS69jrw%2F3FXPG3bNVKuKCWYFm37uJpDUVQn7mOdxzHheXKqax6srRg3EYGaADYyWXGoin8hV0NiMUqLisM4eZXMzXndeoGJ0a5z8bomhcheDsrkS%2B4wrHzN3ClfkJfM0ishFEndGBw1TkSwRMMsYI5aqKC%2BrVWSZp%2FAnJMJeGjIdcLd%2FjQjxCEJ82Y4JxWP11dMCrIYLaG9o1Ez2WT%2BcAGxwauW3XuWUZ45vjfFKOboqeJIPaJavw7RBXNWIhZ9Cwuww7GlQf6Xva1FtSFr%2F3OLHKVwkbh30jrY3nDcq%2B%2FddYsF7TP1Q8Tx5z836oH4TXoGn7lPu8IuIf7vsT82%2FlPb3RatArNFXF%2BhxzPNhhSIUkNdjySxBhxEaPE5Tvu7F6Zaz52xmrrBxgs%2FAgqW7CoQ3mdeAMMwWCkwU52y8GU452kzBvLH%2FJu7gu7gNss4AmGOsLDQaKfbwtJbclUVuOP3Ls75fcPEPtF33YKSoN5tVocL8w%2BC0NIQG0jPrRBqSg%2F2W4DOK65ydM0yxrQpNFZQO6oW7EagD%2FBxOaIDa55Su1T0nPt2pFrtYhaRuDfKGfNNHOAhHzuwjO%2BM9MFgo5SYDqcR0UCR%2F%2FntpjAhf%2FdCNRtdg8lEKcb%2FIKAAVM3zwh4DtlyNMeRRhdeU17AYYJXH%2B9wQjzL7WLUiXPwr4uyLgg2M3cXBFmVpKA%2B%2BGbiWRnmKcqQxrhf8QKcNOEir%2FCgKx3uk13pl14b3UA2re5v7Bl43LA8ChqpPDdu%2BYA1SWUY2og%2FZpH8MOGAnasGOpsBbC76XwClD0m4SFvdftHbf42h384czXXpTCSldc%2FhYR2DAiJ0QlOZaRyvjqW4%2Fdbc%2BL%2FEl6S1Qj%2FCT%2FWlvP53ZSbUiFmPkGp6LwLztrPjZ8Xe%2FIWZF5Wk6g5pExiU2U9sE48Th7NjwxL4oCBhGnaVsfhViDsZdHAH2V3whLGRalMzcNjl5%2Fi0Fx2%2BDuAGZCVwbxUKuJ17xmjx%2Fu4%3D&X-Amz-SignedHeaders=host&X-Amz-Signature=4fca47843a89b38444693ad0b33b4591188df24e677f3573af5614c044f52f59' 
+                    target='_blank' 
+                    rel='noreferrer'
                 >
                     <button className='
                         bg-[#45FFCA] 
@@ -199,7 +202,7 @@ function UserInfo() {
                         max-[426px]:py-[0.75rem]'
                     >Download CV
                     </button>
-                </div>
+                </a>
 
                 <div className="
                     socials-container 
